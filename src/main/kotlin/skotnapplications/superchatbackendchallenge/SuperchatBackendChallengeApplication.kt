e9ab8bf1bf18f0c27aca4e59a0740988082541a5
skotn <mat.skotn@live.no>
2021-09-16 13:39:25 +0200

    Initial spring setup

---
+++ src/main/kotlin/skotnapplications/superchatbackendchallenge/SuperchatBackendChallengeApplication.kt
@@ -0,0 +1,15 @@
+package skotnapplications.superchatbackendchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+
+@SpringBootApplication
+class SuperchatBackendChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SuperchatBackendChallengeApplication>(*args)
+}
