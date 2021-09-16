b6f379346b496cd96c969d2e6d76d900e55a99ac
skotn <mat.skotn@live.no>
2021-09-16 13:52:02 +0200

    Controller functionality for adding contacts and messages

---
+++ src/main/kotlin/skotnapplications/superchatbackendchallenge/controller/Controller.kt
@@ -0,0 +1,37 @@
+package skotnapplications.superchatbackendchallenge.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import skotnapplications.superchatbackendchallenge.model.*
+import skotnapplications.superchatbackendchallenge.repository.ContactRepository
+import skotnapplications.superchatbackendchallenge.repository.MessageRepository
+
+@RestController
+class Controller {
+
+	@Autowired
+	lateinit var messageRepository: MessageRepository
+
+	@Autowired
+	lateinit var contactRepository: ContactRepository
+
+	@GetMapping("/contacts")
+	fun getAllContacts(): ResponseEntity<List<Contact>> {
+		val contacts = contactRepository.findAll()
+		return ResponseEntity(contacts, HttpStatus.OK)
+	}
+
+	@PostMapping("/messages")
+	fun postMessage(@RequestBody message: Message): ResponseEntity<Message> {
+		val createdMessage = messageRepository.save(Message(message.sender, message.receiver, message.content))
+		return ResponseEntity(createdMessage, HttpStatus.CREATED)
+	}
+
+	@PostMapping("/contacts")
+	fun createContact(@RequestBody contact: Contact): ResponseEntity<Contact> {
+		val createdContact = contactRepository.save(Contact(contact.name, contact.mail))
+		return ResponseEntity(createdContact, HttpStatus.CREATED)
+	}
+}
